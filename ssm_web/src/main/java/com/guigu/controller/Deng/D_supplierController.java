14f6b163baca8ada5179eb3f24cfaf64328351c9
dell <547727151@qq.com>
2021-05-12 11:09:47 +0800

    老邓头的demo

--- ssm_web/src/main/java/com/guigu/controller/Deng/D_supplierController.java
+++ ssm_web/src/main/java/com/guigu/controller/Deng/D_supplierController.java
@@ -1,11 +1,14 @@
 package com.guigu.controller.Deng;
 
 
+import com.guigu.mapper.Deng.D_gongyingshangMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
 public class D_supplierController {
-
+    @Autowired
+    D_gongyingshangMapper d_gongyingshangMapper;
 }
